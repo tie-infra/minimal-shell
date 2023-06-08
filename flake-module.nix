01234fcb824cfaf7d30d4c44df42f0812a3debaa
Ivan Trubach <mr.trubach@icloud.com>
2023-06-08 18:55:46 +0300

    feat: allow settings environment variables

--- flake-module.nix
+++ flake-module.nix
@@ -2,7 +2,7 @@
 let
   inherit (flake-parts-lib) mkPerSystemOption;
 
-  mkMinimalShell = pkgs: name: programs: builtins.derivation {
+  mkMinimalShell = pkgs: name: programs: exports: builtins.derivation {
     inherit (pkgs) system;
     inherit name;
 
@@ -11,7 +11,7 @@ let
     outputs = [ "out" ];
 
     stdenv = pkgs.writeTextFile {
-      name = "setup";
+      name = "setup-${name}";
       executable = true;
       destination = "/setup";
       text = ''
@@ -21,22 +21,51 @@ let
         export -n NIX_BUILD_CORES NIX_STORE
         export -n IN_NIX_SHELL
         PATH=${lib.makeBinPath programs}
+        ${lib.concatLines (map (env: "export ${lib.escapeShellArg env}") exports)}
       '';
     };
   };
+
+  packageListType = lib.types.listOf lib.types.package;
+
+  minimalShellType = lib.types.either packageListType (lib.types.submodule {
+    options = {
+      packages = lib.mkOption {
+        type = packageListType;
+        default = [ ];
+        description = lib.mdDoc ''
+          Packages to add to the PATH environment variable.
+        '';
+      };
+      exports = lib.mkOption {
+        type = lib.types.listOf lib.types.str;
+        default = [ ];
+        description = lib.mdDoc ''
+          Additional environment variables to export.
+        '';
+      };
+    };
+  });
 in
 {
   options = {
     perSystem = mkPerSystemOption ({ pkgs, config, ... }: {
       options.minimalShells = lib.mkOption {
-        type = lib.types.lazyAttrsOf (lib.types.listOf lib.types.package);
+        type = lib.types.lazyAttrsOf minimalShellType;
         default = { };
         description = lib.mdDoc ''
           Configure `devShells` with minimal shell environment for direnv.
         '';
       };
 
-      config.devShells = lib.mapAttrs (name: programs: mkMinimalShell pkgs (lib.strings.sanitizeDerivationName name) programs) config.minimalShells;
+      config.devShells = lib.mapAttrs
+        (name: cfg:
+          mkMinimalShell pkgs
+            (lib.strings.sanitizeDerivationName name)
+            (cfg.packages or cfg)
+            (cfg.exports or [ ])
+        )
+        config.minimalShells;
     });
   };
 }
--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   description = "A module for flake-parts to set up minimal devShells outputs.";
-  outputs = _: let flakeModule = import ./flake-module.nix; in {
-    inherit flakeModule;
-    flakeModules.default = flakeModule;
-  };
+  outputs = _:
+    let flakeModule = import ./flake-module.nix; in {
+      inherit flakeModule;
+      flakeModules.default = flakeModule;
+    };
 }
